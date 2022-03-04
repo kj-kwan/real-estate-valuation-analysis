c6914eb8795d477fba49d23c65993e381a78e66c
Kevin Kwan <kj.kwan88@gmail.com>
2022-03-03 21:12:18 -0700

    adding mls urls

---
+++ url_mls_searches.py
@@ -0,0 +1,35 @@
+mls_search_urls = [
+    {
+     'title': 'lt500k Downtown',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=4077049XXbcJ&p=CS-1999394-0#1'
+    },
+    {
+     'title': 'lt425k SWNW Condos',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?ID=0-309658072-10&eml=a2oua3dhbjg4QGdtYWlsLmNvbQ==#1'
+    },
+    {
+     'title': 'NW Calgary',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=1136748XBTPN&p=CS-2073354-0#1',
+     'communities': 'Arbour Lake, Royal Oak, Edgemont, Hamptons, Evanston, Livingston, Carrington, Nolan Hill, Citadel, Hawkwood, Scenic Acres, Ranchlands, Dalhousie, Sage Hill, Panorama Hills, Kincora, Hidden Valley, Simons Valley, Sherwood, Country Hills, Country Hills Village, Coventry Hills, MacEwan, Sandstone Valley, Beddington, Beddington Heights, Silver Springs, Tuscany, Rocky Ridge, Harvest Hills, Huntington Hills, North Haven Upper, North Haven, Thorncliffe'
+    },
+    {
+     'title': 'NW Midcity Calgary',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=1136748XBTPN&p=CS-2073360-0#1',
+     'communities': 'Varsity, Brentwood, Charleswood, Triwood, Bowness, Montgomery, University, University Heights, Point McKay, Parkdale, Saint Andrews Heights, Banff Trail, Capitol Hill, Rosemont, Cambrian Heights, Highwood, Highland Park, Tuxedo Park, Mount Pleasant'
+    },
+    {
+     'title': 'NW Inner City Calgary',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=1136748XBTPN&p=CS-2073362-0#1',
+     'communities': 'West Hillhurst, Upper Hillhurst, Hillhurst, Hounsfield, Briar Hill, Westmount, Rosedale, Sunnyside, Crescent Heights, Renfrew, Bridgeland/Riverside'
+    },
+    {
+     'title': 'Downtown Calgary',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=1136748XBTPN&p=CS-2073370-0#1',
+     'communities': 'Downtown, Downtown West End, Chinatown, Eau Claire, Downtown East Village, Sunalta, Beltline, Connaught, Victoria Park, Lower Mount Royal, Upper Mount Royal, Cliff Bungalow, Mission, Ramsay, Mills Estate, Erlton'
+    },
+    {
+     'title': 'SW Inner City Calgary',
+     'url': 'https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?L=1&k=1136748XBTPN&p=CS-2077172-0',
+     'communities': 'Altadore,Bankview,Bankview,Britannia,Currie Barracks,Currie Barracks,Elbow Park,Elboya,Glamorgan,Glenbrook,Glendale,Glendale,Glendale,Glendale,Glendale,Killarney,Killarney/Glengarry,Knob Hill Acres,Lincoln Park,Lincoln Park,North Glenmore Park,Parkhill,Richmond,Rideau Park,Rosscarrock,Rutland Park,South Calgary,Spruce Cliff,Westgate,Westgate,Westgate,Wildwood,Windsor Park'
+    },
+]
